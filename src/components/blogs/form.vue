2221d03bcc800fc372f382f460fd579e6f3b7031
Aguru Itabashi <itabashi.a@gmail.com>
2025-03-18 00:21:41 +0900

    Formコンポーネントを実装

---
+++ src/components/blogs/form.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { axiosInstance } from '@/utils/axios'
+import { ref } from 'vue'
+
+const title = ref('')
+const contents = ref('')
+
+const emit = defineEmits(['onSubmit'])
+
+const handleSubmit = async () => {
+  await axiosInstance.post('/blogs', { title: title.value, contents: contents.value })
+  emit('onSubmit')
+}
+</script>
+
+<template>
+  <div>
+    <h2>【タイトルを入力してください】</h2>
+    <input class="title" v-model="title" />
+  </div>
+  <div>
+    <h2>【本文を入力してください】</h2>
+    <textarea class="contents" v-model="contents" />
+  </div>
+  <button class="button" @click="handleSubmit">記事を作成</button>
+</template>
--- src/views/BlogCreate.vue
+++ src/views/BlogCreate.vue
@@ -1,28 +1,16 @@
 <script setup lang="ts">
-import { axiosInstance } from '@/utils/axios'
-import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import Form from '@/components/blogs/form.vue'
 
-const title = ref('')
-const contents = ref('')
 const router = useRouter()
 
-const onClick = async () => {
-  await axiosInstance.post('/blogs', { title: title.value, contents: contents.value })
+const handleSubmit = async () => {
   router.push({ name: 'blogList' })
 }
 </script>
 <template>
   <h1>ブログ作成</h1>
-  <div>
-    <h2>【タイトルを入力してください】</h2>
-    <input class="title" v-model="title" />
-  </div>
-  <div>
-    <h2>【本文を入力してください】</h2>
-    <input class="contents" v-model="contents" />
-  </div>
-  <button class="button" @click="onClick">記事を作成</button>
+  <Form @onSubmit="handleSubmit" />
 </template>
 
 <style scoped>
